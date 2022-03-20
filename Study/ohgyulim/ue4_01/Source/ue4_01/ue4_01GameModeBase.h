6fd7dd23d6eb7b8e221dc9c290a844035ca3f8a4
ohgyulim <ohrbfla1004@khu.ac.kr>
2022-03-20 18:56:35 +0900

    UE4 C++ 프로그래밍 입문 (1) - 언리얼 프로그래밍 개요 / C++ 클래스 생성 / 기본 이벤트 | 언리얼 4

---
+++ Study/ohgyulim/ue4_01/Source/ue4_01/ue4_01GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ue4_01GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_01_API Aue4_01GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
