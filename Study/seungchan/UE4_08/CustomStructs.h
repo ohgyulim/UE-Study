749ffc886d6f6f1462358e3205a22e159bbf9b79
sschan99 <seungchan1219@gmail.com>
2022-03-26 07:42:39 +0900

    언리얼 커스텀 구조체 만들기 | 언리얼 4

---
+++ Study/seungchan/UE4_08/CustomStructs.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CustomStructs.generated.h"
+
+/**
+ * 
+ */
+class UE4_08_API CustomStructs
+{
+public:
+	CustomStructs();
+	~CustomStructs();
+};
+
+USTRUCT(Atomic, BlueprintType)
+struct FCustomStruct
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	AActor* actor;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 i;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float f;
+};
