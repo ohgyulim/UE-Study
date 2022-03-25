81d072dcbba526a24c002785efae714605b45d91
sschan99 <seungchan1219@gmail.com>
2022-03-26 02:52:06 +0900

    언리얼 C++ 프로그래밍 헤더 선언하기 | 언리얼 4

---
+++ Study/seungchan/UE4_06/UE4_06GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_06GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_06_API AUE4_06GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
