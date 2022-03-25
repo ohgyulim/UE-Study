730504287bea2878a8c1aa0dd6dbf986d8289bc0
ohgyulim <ohrbfla1004@khu.ac.kr>
2022-03-25 22:18:39 +0900

    언리얼 커스텀 구조체 만들기 | 언리얼 4

---
+++ Study/ohgyulim/ue4_08/Source/ue4_08GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ue4_08GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_08_API Aue4_08GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
