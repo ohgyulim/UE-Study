cc1de756f36654fb57b8a998723e3e0036095514
sschan99 <seungchan1219@gmail.com>
2022-03-28 02:05:18 +0900

    튜토리얼 제대로 따라가기 (5) - 컴포넌트와 콜리전 | 언리얼 4

---
+++ Study/seungchan/UE4_12/RevolutionSceneComponent.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RevolutionSceneComponent.h"
+
+// Sets default values for this component's properties
+URevolutionSceneComponent::URevolutionSceneComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+	UStaticMeshComponent* Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RevolutionMesh"));
+	Mesh->SetupAttachment(this);
+	Mesh->SetRelativeLocation(FVector(0.0f, 100.0f, 0.0f));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+	if (MeshAsset.Succeeded())
+	{
+		Mesh->SetStaticMesh(MeshAsset.Object);
+	}
+}
+
+
+// Called when the game starts
+void URevolutionSceneComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void URevolutionSceneComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+	FRotator NewRotation = GetComponentRotation();
+	NewRotation.Yaw += DeltaTime * 100.0f;
+	SetWorldRotation(NewRotation);
+}
+
