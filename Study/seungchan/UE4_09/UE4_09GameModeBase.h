fa3b99df0b49af0f14aeadc4d110ab153e0fc060
sschan99 <seungchan1219@gmail.com>
2022-03-27 21:41:52 +0900

    튜토리얼 제대로 따라가기 (3) - 게임 조종 카메라 | 언리얼 4

---
+++ Study/seungchan/UE4_09/UE4_09GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_09GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_09_API AUE4_09GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
