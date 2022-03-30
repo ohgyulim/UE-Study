192a3ef5032015af5bde5d0639beac10dfd5e5ee
ohgyulim <ohrbfla1004@khu.ac.kr>
2022-03-30 18:09:44 +0900

    C++ 생성자에서 프로젝트에 포함된 블루프린트 클래스와 리소스 불러오기 | 언리얼 4

---
+++ Study/ohgyulim/ue4_13/Source/ue4_13GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ue4_13GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_13_API Aue4_13GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
