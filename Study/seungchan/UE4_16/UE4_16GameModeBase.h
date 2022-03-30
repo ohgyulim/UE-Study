a78529da25275df3723b78d88461ef93d974dc43
sschan99 <seungchan1219@gmail.com>
2022-03-30 14:50:14 +0900

    뷰포트에 로그 띄우기 | 언리얼 4

---
+++ Study/seungchan/UE4_16/UE4_16GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_16GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_16_API AUE4_16GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
