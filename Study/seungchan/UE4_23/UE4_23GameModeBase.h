2b4d42c3d308b840d76ff0e7474791c8480120bb
sschan99 <seungchan1219@gmail.com>
2022-04-10 00:00:22 +0900

    클릭한 위치로 캐릭터 이동시키는 기능 구현하기 | 언리얼 4

---
+++ Study/seungchan/UE4_23/UE4_23GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_23GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_23_API AUE4_23GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
