73fb56b8f42c254fdbc3646bbaeb9c6394064b0c
sschan99 <seungchan1219@gmail.com>
2022-04-10 01:06:07 +0900

    탑다운 슈팅 컨트롤 구현하기 | 언리얼 4

---
+++ Study/seungchan/UE4_24/UE4_24GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_24GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_24_API AUE4_24GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
